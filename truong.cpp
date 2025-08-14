bea0f8a59556c11cbf3e3b66196d5c0db28f7d69
PhamTruong09 <namphamcong73@gmail.com>
2025-08-15 05:18:12 +0700

    truong

--- truong.cpp
+++ truong.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
-
+using namespace std;
 int main(){
     printf("pham truong");
+    int a,b,c;
+    do
+    {
+    cout<<"nhập 3 giá trị ";
+    cin>>a>>b>>c;
+    } while(a==b||a==c||b==c);
+    if(a>b&&a>c)
+        cout<<"a là số lớn nhất";
+    else
+        if(b>c&&b>a)
+            cout<<"b là số lớn nhất";
+        else 
+            cout<<" c là số lớn nhất";    
+
+    
 }
